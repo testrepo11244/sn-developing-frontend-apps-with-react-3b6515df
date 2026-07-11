5a75511599bbe868f17ee620ab2292aaff0371a2
testrepo11244 <vivocaloid@gmail.com>
2026-07-12 05:22:58 +0700

    add src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import ProductList from './components/ProductList';
 import CartItem from './components/CartItem';
 import './App.css';
 
-const Navbar = () => {
-  const totalItems = useSelector(state =>
-    state.cart.items.reduce((sum, item) => sum + item.quantity, 0)
-  );
-
+function LandingPage() {
   return (
-    <nav className="navbar">
-      <Link to="/" className="nav-link">Home</Link>
-      <Link to="/products" className="nav-link">Plants</Link>
-      <Link to="/cart" className="nav-link cart-link">
-        Cart {totalItems > 0 && <span className="cart-count">{totalItems}</span>}
-      </Link>
-    </nav>
-  );
-};
-
-const LandingPage = () => {
-  return (
-    <div className="landing">
+    <div className="landing-page">
       <h1>Paradise Nursery</h1>
-      <p>Your one‑stop shop for beautiful houseplants</p>
-      <Link to="/products" className="get-started-btn">Get Started</Link>
+      <p>Your one-stop shop for beautiful houseplants</p>
+      <Link to="/products">
+        <button className="get-started-btn">Get Started</button>
+      </Link>
     </div>
   );
-};
+}
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/products" element={<ProductList />} />
